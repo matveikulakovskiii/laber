3dcec6e9c4ee2f74a1fec436b0cb3e6c6e14874f
matveikulakovskiii <120181570+matveikulakovskiii@users.noreply.github.com>
2023-11-14 12:11:47 +0200

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,95 @@
-namespace laber
+﻿namespace laber
 {
     public partial class Form1 : Form
     {
+        GroupBox gp;
+        RadioButton r1;
+        RadioButton r2;
+        Label lbl;
+        TextBox txt_box;
+        Button btn1;
+        Button btn2;
         public Form1()
         {
-            InitializeComponent();
+            this.Height = 250;
+            this.Width = 300;
+
+            
+
+            r1 = new RadioButton();
+            r1.Text = "Раздел 1";
+            r1.Location = new Point(15, 35);
+            r1.Size = new Size(100, 20);
+            r2 = new RadioButton();
+            r2.Text = "Раздел 2";
+            r2.Location = new Point(15, 65);
+            r2.Size = new Size(100, 20);
+            r1.Visible = true;
+            r2.Visible = true;
+            r1.BackColor = Color.GreenYellow;
+            r2.BackColor = Color.GreenYellow;
+            this.Controls.Add(r1);
+            this.Controls.Add(r2);
+
+            gp = new GroupBox();
+            gp.BackColor = Color.GreenYellow;
+            gp.Text = "Добавить в";
+            gp.Location = new Point(10, 10);
+            gp.Size = new Size(260, 100);
+            this.Controls.Add(gp);
+
+            lbl = new Label();
+            lbl.Text = "Введите текст:";
+            lbl.ForeColor = Color.Black;
+            lbl.Location = new Point(10, 120);
+            lbl.Size = new Size(90, 20);
+            lbl.Visible = true;
+            lbl.BackColor = Color.LightBlue;
+            this.Controls.Add(lbl);
+
+            txt_box = new TextBox();
+            txt_box.BorderStyle = BorderStyle.Fixed3D;
+            txt_box.Height = 20;
+            txt_box.Width = 260;
+            txt_box.Text = "";
+            txt_box.Location = new Point(10, 145);
+            this.Controls.Add(txt_box);
+
+            btn1 = new Button();
+            btn1.Height = 30;
+            btn1.Width = 120;
+            btn1.Text = "Добавить";
+            btn1.Location = new Point(10, 175);
+            btn1.Visible = true;
+            btn1.Click += new System.EventHandler(ButtonCreate);
+            this.Controls.Add(btn1);
+            void ButtonCreate(object sender, EventArgs e)
+            {
+                Forms Main = this.Owner as Forms;
+
+                if (txt_box.Text != "")
+                {
+                    if (this.r1.Checked == true)
+                        Main.lb.Items.Add(this.txt_box.Text);
+                    if (this.r2.Checked == true)
+                        Main.lb1.Items.Add(this.txt_box.Text);
+
+                    this.Close();
+                }
+            }
+
+            btn2 = new Button();
+            btn2.Height = 30;
+            btn2.Width = 120;
+            btn2.Text = "Отмена";
+            btn2.Location = new Point(150, 175);
+            btn2.Click += new System.EventHandler(ButtonExit);
+            btn2.Visible = true;
+            this.Controls.Add(btn2);
+            void ButtonExit(object sender, EventArgs e)
+            {
+                this.Close();
+            }
         }
     }
 }
--- Forms.cs
+++ Forms.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.VisualBasic.ApplicationServices;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -23,8 +25,8 @@ namespace laber
         Panel pnl;
         ComboBox cb;
         ComboBox cb1;
-        ListBox lb;
-        ListBox lb1;
+        public ListBox lb;
+        public ListBox lb1;
         ListBox lb2;
         RichTextBox rtb;
         Button btn1;
@@ -140,15 +142,6 @@ namespace laber
             rtb.Size = new Size(350, 470);
             this.Controls.Add(rtb);
 
-            
-            
-
-
-
-
-
-
-
             lbl = new Label();
             lbl.Text = "Раздел 1";
             lbl.ForeColor = Color.Black;
@@ -213,7 +206,18 @@ namespace laber
             btn1.Text = ">";
             btn1.Location = new Point(200, 100);
             btn1.Visible = true;
+            btn1.Click += new System.EventHandler(ButtonToRigth);
             this.Controls.Add(btn1);
+            void ButtonToRigth(object sender, EventArgs e)
+            {
+                lb1.BeginUpdate();
+                foreach (object Item in lb.SelectedItems)
+                {
+                    lb1.Items.Add(Item);
+                }
+                lb.Items.RemoveAt(lb.SelectedIndex);
+                lb1.EndUpdate();
+            }
 
             btn2 = new Button();
             btn2.Height = 30;
@@ -221,23 +225,46 @@ namespace laber
             btn2.Text = "<";
             btn2.Location = new Point(200, 140);
             btn2.Visible = true;
+            btn2.Click += new System.EventHandler(ButtonToLeft);
             this.Controls.Add(btn2);
+            void ButtonToLeft(object sender, EventArgs e)
+            {
+                lb.BeginUpdate();
+                foreach (object Item in lb1.SelectedItems)
+                {
+                    lb.Items.Add(Item);
+                }
+                lb1.Items.RemoveAt(lb1.SelectedIndex);
+                lb.EndUpdate();
+            }
 
             btn3 = new Button();
             btn3.Height = 30;
             btn3.Width = 100;
             btn3.Text = ">>";
             btn3.Location = new Point(200, 180);
+            btn3.Click += new System.EventHandler(ButtonToAllRigth);
             btn3.Visible = true;
             this.Controls.Add(btn3);
+            void ButtonToAllRigth(object sender, EventArgs e)
+            {
+                lb1.Items.AddRange(lb.Items);
+                lb.Items.Clear();
+            }
 
             btn4 = new Button();
             btn4.Height = 30;
             btn4.Width = 100;
-            btn4.Text = ">>";
+            btn4.Text = "<<";
             btn4.Location = new Point(200, 220);
             btn4.Visible = true;
+            btn4.Click += new System.EventHandler(ButtonToAllLeft);
             this.Controls.Add(btn4);
+            void ButtonToAllLeft(object sender, EventArgs e)
+            {
+                lb.Items.AddRange(lb1.Items);
+                lb1.Items.Clear();
+            }
 
             btn5 = new Button();
             btn5.Height = 30;
@@ -245,7 +272,63 @@ namespace laber
             btn5.Text = "Сортировать";
             btn5.Location = new Point(50, 310);
             btn5.Visible = true;
+            btn5.Click += new System.EventHandler(ButtonSortlb1);
             this.Controls.Add(btn5);
+            void ButtonSortlb1(object sender, EventArgs e)
+            {
+                if (cb.Text == "Алфавит (по возрастанию)")
+                    lb.Sorted = true;
+                else if (cb.Text == "Алфавит (по убыванию)")
+                {
+                    lb.Sorted = false;
+                    ArrayList list = new ArrayList();
+                    foreach (var item in lb.Items)
+                    {
+                        list.Add(item);
+                    }
+                    list.Sort();
+                    list.Reverse();
+                    lb.Items.Clear();
+                    foreach (var item in list)
+                    {
+                        lb.Items.Add(item);
+                    }
+                }
+                else if (cb.Text == "Длине слова (по возрастанию)")
+                {
+                    lb.Sorted = false;
+                    for (int i = 0; i < lb.Items.Count; i++)
+                    {
+                        for (int j = i; j < lb.Items.Count; j++)
+                        {
+                            if (lb.Items[i].ToString().Length > lb.Items[j].ToString().Length)
+                            {
+                                string temp;
+                                temp = lb.Items[i].ToString();
+                                lb.Items[i] = lb.Items[j];
+                                lb.Items[j] = temp;
+                            }
+                        }
+                    }
+                }
+                else if (cb.Text == "Длине слова (по убыванию)")
+                {
+                    lb.Sorted = false;
+                    for (int i = 0; i < lb.Items.Count; i++)
+                    {
+                        for (int j = i; j < lb.Items.Count; j++)
+                        {
+                            if (lb.Items[j].ToString().Length > lb.Items[i].ToString().Length)
+                            {
+                                string temp;
+                                temp = lb.Items[j].ToString();
+                                lb.Items[j] = lb.Items[i];
+                                lb.Items[i] = temp;
+                            }
+                        }
+                    }
+                }
+            }
 
             btn6 = new Button();
             btn6.Height = 30;
@@ -253,7 +336,12 @@ namespace laber
             btn6.Text = "Очистить";
             btn6.Location = new Point(50, 350);
             btn6.Visible = true;
+            btn6.Click += new System.EventHandler(ButtonClear);
             this.Controls.Add(btn6);
+            void ButtonClear(object sender, EventArgs e)
+            {
+                lb.Items.Clear();
+            }
 
             btn7 = new Button();
             btn7.Height = 30;
@@ -261,7 +349,15 @@ namespace laber
             btn7.Text = "Добавить";
             btn7.Location = new Point(200, 310);
             btn7.Visible = true;
+            btn7.Click += new System.EventHandler(ButtonCreate);
             this.Controls.Add(btn7);
+            void ButtonCreate(object sender, EventArgs e)
+            {
+                Form1 AddRec = new Form1();
+
+                AddRec.Owner = this;
+                AddRec.ShowDialog();
+            }
 
             btn8 = new Button();
             btn8.Height = 30;
@@ -269,7 +365,15 @@ namespace laber
             btn8.Text = "Удалить";
             btn8.Location = new Point(200, 350);
             btn8.Visible = true;
+            btn8.Click += new System.EventHandler(ButtonDeleteSelected);
             this.Controls.Add(btn8);
+            void ButtonDeleteSelected(object sender, EventArgs e)
+            {
+                if (lb1.Text == "")
+                    lb.Items.RemoveAt(lb.SelectedIndex);
+                if (lb.Text == "")
+                    lb1.Items.RemoveAt(lb1.SelectedIndex);
+            }
 
             btn9 = new Button();
             btn9.Height = 30;
@@ -277,7 +381,63 @@ namespace laber
             btn9.Text = "Сортировать";
             btn9.Location = new Point(350, 310);
             btn9.Visible = true;
+            btn9.Click += new System.EventHandler(ButtonSortlb2);
             this.Controls.Add(btn9);
+            void ButtonSortlb2(object sender, EventArgs e)
+            {
+                if (cb1.Text == "Алфавит (по возрастанию)")
+                    lb1.Sorted = true;
+                else if (cb1.Text == "Алфавит (по убыванию)")
+                {
+                    lb1.Sorted = false;
+                    ArrayList list = new ArrayList();
+                    foreach (var item in lb1.Items)
+                    {
+                        list.Add(item);
+                    }
+                    list.Sort();
+                    list.Reverse();
+                    lb1.Items.Clear();
+                    foreach (var item in list)
+                    {
+                        lb1.Items.Add(item);
+                    }
+                }
+                else if (cb1.Text == "Длине слова (по возрастанию)")
+                {
+                    lb1.Sorted = false;
+                    for (int i = 0; i < lb1.Items.Count; i++)
+                    {
+                        for (int j = i; j < lb1.Items.Count; j++)
+                        {
+                            if (lb1.Items[i].ToString().Length > lb1.Items[j].ToString().Length)
+                            {
+                                string temp;
+                                temp = lb1.Items[i].ToString();
+                                lb1.Items[i] = lb1.Items[j];
+                                lb1.Items[j] = temp;
+                            }
+                        }
+                    }
+                }
+                else if (cb1.Text == "Длине слова (по убыванию)")
+                {
+                    lb1.Sorted = false;
+                    for (int i = 0; i < lb1.Items.Count; i++)
+                    {
+                        for (int j = i; j < lb1.Items.Count; j++)
+                        {
+                            if (lb1.Items[j].ToString().Length > lb1.Items[i].ToString().Length)
+                            {
+                                string temp;
+                                temp = lb1.Items[j].ToString();
+                                lb1.Items[j] = lb1.Items[i];
+                                lb1.Items[i] = temp;
+                            }
+                        }
+                    }
+                }
+            }
 
             btn10 = new Button();
             btn10.Height = 30;
@@ -285,7 +445,12 @@ namespace laber
             btn10.Text = "Очистить";
             btn10.Location = new Point(350, 350);
             btn10.Visible = true;
+            btn10.Click += new System.EventHandler(ButtonClear1);
             this.Controls.Add(btn10);
+            void ButtonClear1(object sender, EventArgs e)
+            {
+                lb1.Items.Clear();
+            }
 
             lbl2 = new Label();
             lbl2.Text = "Введи искомое слово";
@@ -330,14 +495,47 @@ namespace laber
             btn11.Text = "Поиск";
             btn11.Location = new Point(210, 550);
             btn11.Visible = true;
+            btn11.Click += new System.EventHandler(ButtonSearch);
             this.Controls.Add(btn11);
+            void ButtonSearch(object sender, EventArgs e)
+            {
+                lb2.Items.Clear();
+
+                string Find = txt_box.Text;
+
+                if (c1.Checked)
+                {
+                    foreach (string String in lb.Items)
+                    {
+                        if (String.Contains(Find)) lb2.Items.Add(String);
+                    }
+                }
+                if (c2.Checked)
+                {
+                    foreach (string String in lb1.Items)
+                    {
+                        if (String.Contains(Find)) lb2.Items.Add(String);
+                    }
+                }
+                if (c1.Checked && c2.Checked)
+                {
+                    foreach (string String in lb1.Items)
+                    {
+                        if (String.Contains(Find)) lb2.Items.Add(String);
+                    }
+                    foreach (string String in lb.Items)
+                    {
+                        if (String.Contains(Find)) lb2.Items.Add(String);
+                    }
+                }
+            }
 
             gp = new GroupBox();
             gp.BackColor = Color.Orange;
             gp.Text = "Поиск";
             gp.Location = new Point(20, 400);
             gp.Size = new Size(300, 230);
-            this.Controls.Add(gp);
+            this.Controls.Add(gp);  
 
             btn12 = new Button();
             btn12.Height = 70;
@@ -345,7 +543,21 @@ namespace laber
             btn12.Text = "Сброс";
             btn12.Location = new Point(330, 480);
             btn12.Visible = true;
+            btn12.Click += new System.EventHandler(ButtonRe);
             this.Controls.Add(btn12);
+            void ButtonRe(object sender, EventArgs e)
+            {
+                lb.Items.Clear();
+                lb1.Items.Clear();
+                lb2.Items.Clear();
+                rtb.Text = string.Empty;
+                txt_box.Text = string.Empty;
+                r1.Checked = false;
+                r2.Checked = false;
+                r3.Checked = false;
+                c1.Checked = false;
+                c2.Checked = false;
+            }
 
             btn13 = new Button();
             btn13.Height = 70;
@@ -353,7 +565,12 @@ namespace laber
             btn13.Text = "Выход";
             btn13.Location = new Point(330, 560);
             btn13.Visible = true;
+            btn13.Click += new System.EventHandler(ButtonExit);
             this.Controls.Add(btn13);
+            void ButtonExit(object sender, EventArgs e)
+            {
+                Application.Exit();
+            }
 
             pnl = new Panel();
             pnl.Location = new System.Drawing.Point(10,30);
@@ -391,7 +608,33 @@ namespace laber
             btn14.Text = "Начать";
             btn14.Location = new Point(760, 550);
             btn14.Visible = true;
+            btn14.Click += new System.EventHandler(ButtonItemsWrite);
             this.Controls.Add(btn14);
+            void ButtonItemsWrite(object sender, EventArgs e)
+            {
+                lb.Items.Clear();
+                lb2.Items.Clear();
+
+                lb.BeginUpdate();
+
+                string[] Strings = rtb.Text.Split(new char[] { '\n', '\t', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string s in Strings) 
+                {
+                    string Str = s.Trim();
+
+                    if (Str == String.Empty) continue;
+                    if (r1.Checked) lb.Items.Add(Str);
+                    if (r2.Checked)
+                    {
+                        if (Regex.IsMatch(Str, @"\d")) lb.Items.Add(Str);
+                    }
+                    if (r3.Checked)
+                    {
+                        if (Regex.IsMatch(Str, @"\w+@\w+\.\w+")) lb.Items.Add(Str);
+                    }
+                }
+                lb.EndUpdate();
+            }
 
             gp1 = new GroupBox();
             gp1.BackColor = Color.GreenYellow;
